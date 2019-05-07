7e2c7d1689a49bbccd73a699d143b646d331fb30
Emmanuel Cobian <emmanuelcobian@la-MacBook-Pro-de-Emmanuel-225.local>
2019-05-07 17:51:59 -0500

    hasheando la contraseña

--- inc/modelos/modelo-admin.php
+++ inc/modelos/modelo-admin.php
@@ -1,11 +1,29 @@
 <?php 
     
-    $acccion = $_POST['accion'];
+    
+    $accion = $_POST['accion'];
     $usuario = $_POST['usuario'];
     $password = $_POST['password'];
-
     if($accion == 'crear'){
         //Codigo para crear administrador
+
+        //Hashear password
+        $opciones = array(
+            'cost' => 12
+        );
+
+        $hashPassword = password_hash($password, PASSWORD_BCRYPT, $opciones);
+
+        //Importar la conexion
+        include '../funciones/conexion.php';
+        $respuesta = array(
+            'pass' => $hashPassword
+        );
+        echo json_encode($respuesta);
+    }
+    if($accion == 'login'){
+        //Codigo para logguear un administrador
+
         
     }
 
