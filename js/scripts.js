6058faff8529e138bffe2295ea5b1fd0a4673e70
Emmanuel Cobian <emmanuelcobian@la-MacBook-Pro-de-Emmanuel-225.local>
2019-05-08 20:57:15 -0500

    Funcion para crear nuevos proyectos

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 eventListeners();
+//Lista de proyectos
+var listaProyectos = document.querySelector('ul#proyectos');
 
 function eventListeners(){
     //Boton para crear proyecto
@@ -8,4 +10,24 @@ function eventListeners(){
 function nuevoProyecto(e){
     e.preventDefault();
     console.log('Presionaste nuevo proyecto');
+    //Crear un input para el nombre del nuevo proyecto
+    var nuevoProyecto = document.createElement('li');
+    nuevoProyecto.innerHTML = "<input type=\"text\" id=\"nuevo-proyecto\">";
+    listaProyectos.appendChild(nuevoProyecto);
+
+    //Seleccionar el id con el nuevo proyecto
+    var inputNuevoProyecto = document.querySelector('#nuevo-proyecto');
+
+    //Al presionar enter crear el proyecto
+    inputNuevoProyecto.addEventListener('keypress', function(e){
+        var tecla = e.wish || e.keyCode;
+        if(tecla == 13){
+            guardarProyectoBD(inputNuevoProyecto.value);
+            listaProyectos.removeChild(nuevoProyecto);
+        }
+    });
+}
+
+function guardarProyectoBD(nombreProyecto){
+    console.log(nombreProyecto);
 }
